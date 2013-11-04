c577c0dee32837e2a6bed166a6db21a552f9931e
cetoli <carlo@nce.ufrj.br>
2013-11-04 12:53:14 -0200

    feat (cave): Início do projeto

---
+++ src/caverna.py
@@ -0,0 +1,42 @@
+"""
+############################################################
+Caverna - Principal
+############################################################
+
+:Author: *Carlo E. T. Oliveira*
+:Contact: carlo@nce.ufrj.br
+:Date: 2013/11/04
+:Status: This is a "work in progress"
+:Revision: 0.1.3
+:Home: `Labase <http://labase.selfip.org/>`__
+:Copyright: 2013, `GPL <http://is.gd/3Udt>`__.
+
+Caverna é um jogo de aventuras em uma caverna.
+"""
+CAVEX = "https://dl.dropboxusercontent.com/u/1751704/labase/caverna/img/cavernax.jpg"
+
+
+class Caverna:
+    """Uma caverna com cameras tuneis e habitantes. :ref:`caverna`
+    """
+    def __init__(self, gui):
+        """Initializes builder and gui. """
+        self.doc = gui.DOC
+        self.html = gui.HTML
+        self.camera = {}
+        self.tunel = {}
+        self.heroi = None
+        self.main = self.doc['main']
+
+    def cria_caverna(self):
+        """Cria a caverna e suas partes."""
+        self.main.style.backgroundSize = 'cover'
+        self.main.style.backgroundImage = 'url(%s)' % CAVEX
+        self.main.style.width = 1000
+        self.main.style.height = 800
+        return self
+
+
+def main(gui):
+    print('Caverna 0.1.0')
+    caverna = Caverna(gui).cria_caverna()
